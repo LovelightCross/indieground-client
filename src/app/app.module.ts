eae7101abd6e3d3432a5a74651512ce333b7dca4
Lovelight Cross <killlovelight@gmail.com>
2017-12-06 14:42:58 -0600

    pull changes (#4)
    * Merge pull request #1 from LovelightCross/master (#2)
    
    * created nav
    
    * added footer
    
    * added onto footer
    
    * last adjustment to header/footer
    
    * made nav and footer look nice
    
    * edited width, etc...
    
    * added component
    
    * deleted navbar and footer (folder) moved to app.component
    
    * edited app module
    
    * added soundcloud api
    
    * added soundcloud widget
    
    * homepage has been changed to home
    
    * fixed home routing
    
    * added artist comp.
    
    * no login page
    
    * Merge pull request #1 from LovelightCross/master (#3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistComponent } from './artist/artist.component';
 
+
+const routes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'artists',
+  component: ArtistsComponent
+},
+{ path: 'artist',
+  component: ArtistComponent
+},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistsComponent,
+    HomeComponent,
+    ArtistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
