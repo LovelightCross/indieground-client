dd4de8165084a22530e810b2504c2b1e8b9183b7
SAC819 <shaniece_cole@yahoo.com>
2017-12-06 14:38:33 -0600

    no login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ArtistsComponent } from './artists/artists.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
 {
   path: '',
-  component: LoginComponent
-},
-{
-  path: 'home',
   component: HomeComponent
 },
 {
@@ -30,7 +25,6 @@ const routes: Routes = [
     AppComponent,
     ArtistsComponent,
     HomeComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
