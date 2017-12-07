6c134cd8fcdec91150b41c119c8e0c4a6a72d97d
Lovelight Cross <killlovelight@gmail.com>
2017-12-07 11:53:46 -0600

    Daily Merge From Dev To Master (#5)
    * pull changes (#4)
    
    * Merge pull request #1 from LovelightCross/master (#2)
    
    * created nav
    
    * added footer
    
    * added onto footer
    
    * last adjustment to header/footer
    
    * made nav and footer look nice
    
    * edited width, etc...
    
    * added component
    
    * deleted navbar and footer (folder) moved to app.component
    
    * edited app module
    
    * added soundcloud api
    
    * added soundcloud widget
    
    * homepage has been changed to home
    
    * fixed home routing
    
    * added artist comp.
    
    * no login page
    
    * Merge pull request #1 from LovelightCross/master (#3)
    
    * added components
    
    * artist gone
    
    * fixed routes
    
    * yeah it happened
    
    * added artists route
    
    * added matIcon to appmodule/ added onto single-artist
    
    * artists page is updating
    
    * artists page looks ok
    
    * changed header and footer styles and attributes
    
    * small changes
    
    * added btn that would lead to single page
    
    * fixed routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material';
+
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgForOf } from '@angular/common';
+
 
 import { AppComponent } from './app.component';
 import { ArtistsComponent } from './artists/artists.component';
 import { HomeComponent } from './home/home.component';
+
+import { SingleArtistComponent } from './single-artist/single-artist.component';
+import { MusicComponent } from './music/music.component';
+import { ShowsComponent } from './shows/shows.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ArtistService } from './services/artist.sv';
+
 
 
 const routes: Routes = [
@@ -18,13 +29,28 @@ const routes: Routes = [
   path: 'artists',
   component: ArtistsComponent
 },
+
+{
+  path: 'single-artist',
+  component: SingleArtistComponent
+},
+{
+  path: 'shows',
+  component: ShowsComponent
+}
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistsComponent,
-    HomeComponent
+    HomeComponent,
+    SingleArtistComponent,
+    MusicComponent,
+    ShowsComponent,
+    ContactUsComponent
+
   ],
   imports: [
     BrowserModule,
@@ -33,9 +59,13 @@ const routes: Routes = [
     BrowserAnimationsModule,
     MatMenuModule,
     MatButtonModule,
-    MatIconModule
+
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ArtistService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
